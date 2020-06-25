0c26bbc1d2f08cccf057198ef8f9dbf7eb4aa441
CakeCrusher <37946988+CakeCrusher@users.noreply.github.com>
2020-06-25 12:35:53 -0400

    improved notification design

--- components/newNavBar/user/NewNotif.js
+++ components/newNavBar/user/NewNotif.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import PostSmall from '../../post/PostSmall'
 import NS from '../../../styles/notif.module.css'
@@ -8,16 +8,16 @@ import Link from 'next/link'
 // components/newNavBar/user
 const Notif = (props) => {
     const n = props.notif
-    const typeIcon = () => {
-        switch (n.type) {
-            case 'join request':
-                return <img className={NS.joinIcon} src="/svg/rocket.svg" />
-            case 'question':
-                return <div className={NS.questionIcon}>?</div>
-            default:
-                return null
-        }
-    }
+    // const typeIcon = () => {
+    //     switch (n.type) {
+    //         case 'join request':
+    //             return <img className={NS.joinIcon} src="/svg/rocket.svg" />
+    //         case 'question':
+    //             return <div className={NS.questionIcon}>?</div>
+    //         default:
+    //             return null
+    //     }
+    // }
     const readStyle = n.read ? {opacity: '0.7'} : null
     let cleanedTime = new Date(Number(n.date))
     cleanedTime = cleanedTime.toString().split(' ')
@@ -26,17 +26,17 @@ const Notif = (props) => {
         <Link href="/">
             <a className="neutralize-link">
                 <div className={NS.notifContainer} style={readStyle}>
-                    <div className={NS.iconNamePair}>
-                        <img src="/svg/astronaut.svg" className={NS.userIcon} />
-                        <p className={NS.userUsername}>{n.user_from.username}</p>
+                    <div className={NS.sideBySide}>
+                        <div className={NS.iconNamePair}>
+                            <img src="/svg/astronaut.svg" className={NS.userIcon} />
+                            <p className={NS.userUsername}>{n.user_from.username}</p>
+                        </div>
+                        <div className={NS.date}>{cleanedTime}</div>
                     </div>
-                    <div className={NS.iconNamePair}>
-                        <p className={NS.typeTitle}>{n.type}</p>
-                        {typeIcon()}
-                    </div>
+                    <p className={NS.typeTitle}>{n.type}</p>
                     <PostSmall post={n.post} />
                     <p className={NS.message}>{n.message}</p>
-                    <div className={NS.date}>{cleanedTime}</div>
+                    
                 </div>
             </a>
         </Link>
