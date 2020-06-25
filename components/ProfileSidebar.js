786fc6ddf9e3be8d0dee1f37ed61bc4cf138854f
CakeCrusher <37946988+CakeCrusher@users.noreply.github.com>
2020-06-25 10:43:55 -0400

    improved design

--- components/ProfileSidebar.js
+++ components/ProfileSidebar.js
@@ -1,33 +1,38 @@
 import React from 'react';
-import style from '../styles/profileSidebar.module.css';
+import PSS from '../styles/profileSidebar.module.css';
 
 const Sidebar = () => (
   <React.Fragment>
-    <div className={style.container}>
-      <div className={style.info}>
-        <img src="/svg/astronaut.svg" className={style.avatar} />
-        <div>
-          <h2>Username</h2>
+    <div className={PSS.container}>
+      <div className={PSS.header}>
+        <div className={PSS.iconTextPair}>
+          <img src="https://avatarfiles.alphacoders.com/560/56030.jpg" className={PSS.avatar} />
+          <div>
+            <h2 className={PSS.username}>Username</h2>
+            <p className={PSS.secondary}>Sebastian Sosa</p>
+          </div>
         </div>
+        <p className={PSS.secondary} style={{marginLeft: '5px'}}>Sebastian Sosa</p>
       </div>
-      <div className={style.projects}>
-        <img src="/svg/rocket.svg" className={style.icon}/>
-        <h3>Projects</h3>
-          <ul>
-            <li><button className={style.viewButton}>Created</button></li>
-            <li><button className={style.viewButton}>Joined</button></li>
-            <li><button className={style.viewButton}>Following</button></li>
-            <li><button className={style.viewButton}>Others</button></li>
-          </ul>
+      <div className={PSS.category}>
+        <div className={PSS.iconTextPair}>
+          <img src="/svg/rocket.svg" className={PSS.icon}/>
+          <h3 className={PSS.categoryTitle}>Projects</h3>
+        </div>
+        <button className={PSS.viewButton}>Created</button>
+        <button className={PSS.viewButton}>Joined</button>
+        <button className={PSS.viewButton}>Following</button>
+        <button className={PSS.viewButton}>Others</button>
+          
       </div>
-      <div className={style.users}>
-        <img src="/svg/astronaut.svg" className={style.icon}/>
-        <h3>Users</h3>
-          <ul>
-            <li><button className={style.viewButton}>In my projects</button></li>
-            <li><button className={style.viewButton}>Invited</button></li>
-            <li><button className={style.viewButton}>Others</button></li>
-          </ul>
+      <div className={PSS.category}>
+        <div className={PSS.iconTextPair}>
+          <img src="/svg/astronaut.svg" className={PSS.icon}/>
+          <h3 className={PSS.categoryTitle}>Users</h3>
+        </div>
+        <button className={PSS.viewButton}>In my projects</button>
+        <button className={PSS.viewButton}>Invited</button>
+        <button className={PSS.viewButton}>Others</button>
       </div>
     </div>
   </React.Fragment>
